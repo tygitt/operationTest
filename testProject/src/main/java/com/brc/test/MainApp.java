6195ef48380fad9ffaa56cf667df5be56ab401ac
tygitt <918342704@qq.com>
2019-11-11 17:11:44 +0800

    git push test

---
+++ testProject/src/main/java/com/brc/test/MainApp.java
@@ -0,0 +1,8 @@
+package com.brc.test;
+
+public class MainApp {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("hello git");
+    }
+}
